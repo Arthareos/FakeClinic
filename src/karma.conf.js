6f8fba7908e48494b18dc070cf917d0994de70ef
Cristi <cristian.m99a@gmail.com>
2020-04-15 00:20:45 +0300

    0.0.1

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/FakeClinic'),
+      dir: require('path').join(__dirname, '../coverage/angular7tutorial'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
