d5e83f3a2d90993bf364e93be51ab5591921e310
Simone Di Fonzo <difonzosimone@hotmail.com>
2020-04-15 02:48:16 +0300

    0.1.1
     - ADDED: Doctor inbox component functionality;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { LandingDoctorComponent } from './landing/landing-doctor/landing-doctor.
 import { LandingPatientComponent } from './landing/landing-patient/landing-patient.component';
 import { AppointmentsComponent } from './landing/landing-doctor/appointments/appointments.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { DoctorInboxComponent } from './landing/landing-doctor/doctor-inbox/doctor-inbox.component';
 
 @NgModule({
     imports: [
@@ -34,7 +35,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
         LandingDoctorComponent,
         LandingPatientComponent,
         AppointmentsComponent,
-        NavbarComponent
+        NavbarComponent,
+        DoctorInboxComponent
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
