161b2cb4d8e7660865cba1c0d8cfb4e91c27b141
Simone Di Fonzo <difonzosimone@hotmail.com>
2020-06-03 18:37:23 +0300

    0.2.6
     - FIXED: Stuff;
     - ADDED: Stuff;
     - REMOVED: Stuff;

--- src/app/landing/landing-patient/ticket-creator/ticket-creator.component.ts
+++ src/app/landing/landing-patient/ticket-creator/ticket-creator.component.ts
@@ -12,4 +12,12 @@ export class TicketCreatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showCreator() {
+    var x = document.getElementById("ticketCreator");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 }
